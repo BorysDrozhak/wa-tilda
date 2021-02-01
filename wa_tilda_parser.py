fea2baad87179b7cb3c8e556d4a1836d6c06e0a4
bdrozhak <borys.drozhak@datarobot.com>
2021-02-01 23:08:54 +0200

    another commit

--- wa_tilda_parser.py
+++ wa_tilda_parser.py
@@ -9,15 +9,15 @@ c = "Zgu_caRA7oHMIp67a8"  # do not even ask why. it is gonna be used by regular 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 a = "165506622"
-updater = Updater(token=a + "2" + ':' + b + c, use_context=True)
-dispatcher = updater.dispatcher
+tok = a + "2" + ':' + b + c
 
+updater = Updater(token=tok, use_context=True)
+dispatcher = updater.dispatcher
 
 def send_parsed_order(update, context):
     context.bot.send_message(
         chat_id=update.effective_chat.id, text=parse_order(update.message.text)
     )
-
 
 class FilterAwesome(MessageFilter):
     def filter(self, message):
@@ -26,6 +26,16 @@ filter_awesome = FilterAwesome()
 
 order_handler = MessageHandler(filter_awesome, send_parsed_order)
 dispatcher.add_handler(order_handler)
+
+# just logging messages recieved
+def echo(update, context):
+    print(update.message.text)
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+
 
 updater.start_polling()
 
@@ -36,9 +46,17 @@ def parse_order(text):
     client_name, client_phone = None, None
     delivery_zone, total_order_price, order_type = None, None, None
     client_comment, persons, client_no_need = None, None, None
-    result_order_block, other = None, None
+    result_order_block, other, utm = None, None, None
 
     order_block = text.split("Данные плательщика:")[0]
+    add_block = text.split("Дополнительные данные:")
+    if len(add_block) > 1:
+        add_block = add_block[1]
+    for line in add_block.split('\n'):
+        if "UTM source:" in line:
+            utm = line
+            break
+
     client_block = (
         text
         .split("Данные плательщика:")[1]
@@ -66,19 +84,18 @@ def parse_order(text):
         elif param == 'не_треба':
             client_no_need = info
         elif param == 'Persons':
-            persons = "For: " + info
-        elif param == 'Comment':
+            persons = "Для: " + info
+        elif param == 'Комент:':
             client_comment = "Comment: " + info
         elif param == 'НЕдзвонити-НАПИСАТИ':
             if info == 'yes':
-                client_nocall = 'NO CALL'
-            else:
-                client_nocall = 'Please CALL the Client!'
+                client_nocall = 'NO CALL\n'
         else:
             other += line
     if not other:
         other = ''
-
+    if not client_nocall:
+        client_nocall = 'Треба дзвонити клієнту!\n'
 
     result_order_block = ''
     for line in order_block.split("\n"):
@@ -115,6 +132,8 @@ def parse_order(text):
             name = name + " (Одинарна упаковка)"
         elif "Розмір порції: XL СЕТ (3 шт в крафтовому боксі з авторським салатом)" in options:
             name = name + " (XL СЕТ 3 шт)"
+        elif "Розмір порції: XS (protein balanced)" in options:
+            name = name + " (XS protein balanced)"
         result_order_block += amount + " x " + name + "\t - (" + total_price + " uah)" + "\n"
         if options:
             for option in options.split(","):
@@ -129,7 +148,11 @@ def parse_order(text):
                     "Розмір порції: Одинарні ( 1 шт в японській упаковці)",
                     "Поливати горіховим соусом?: Так",
                     "Бажаєте більше курячого філе (+70г)?: Ні",
-                    "Потрібні імбир та васабі?: Так"
+                    "Потрібні імбир та васабі?: Так",
+                    "Бажаєте додати до боулу броколі(30г) чи більше лосося(30г)?: Ні",
+                    "дякую",
+                    "Розмір порції: Standard",
+                    "Розмір порції: XS (protein balanced)",
                 ]:
                     continue
                 result_order_block += "    " + option + "\n"
@@ -153,5 +176,9 @@ def parse_order(text):
     parsed_text += [other]
     parsed_text += ['----']
     parsed_text += [result_order_block]
+    if utm:
+        parsed_text += ['----', utm]
 
-    return '\n'.join(parsed_text)
+    return '\n'.join(
+        [i for i in parsed_text if i]
+    )
