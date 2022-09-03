b611f272e3a379542de47b18db5063a6a196bb63
alonewerwolf <andriy.zhigil@gmail.com>
2022-09-03 18:20:22 +0300

    add weather info in total kassa

--- tests/unit/test_rocket.py
+++ tests/unit/test_rocket.py
@@ -3,7 +3,7 @@
 from tests.unit.data.rocket_order_examples import (example1, example_total1,
                                                    example_total2,
                                                    example_total3)
-from utils.rocket import parse_rocket_fmt, parse_total_kassa
+from utils.rocket import parse_rocket_fmt, parse_total_kassa, get_weather
 
 
 def test_example1():
@@ -18,13 +18,15 @@ def test_total_tip():
         'Каса 2021-05-13 - Разом: 13268\n'
         'Доставка: 5458\n'
         'Зал ресторану: 7810\n'
-        'чай: 85.0?'
+        'чай: 85.0?\n'
+        f"{get_weather()}"
     )
 
     assert parse_total_kassa(example_total2) == (
         'Каса 2021-06-03 - Разом: 19671\nДоставка: 8191\nЗал ресторану: 11480'
         '\nНе сходиться z-звіт з айко продажем на:474.5'
-        '\n\nВав! Маю надію ви всі добре почуваєтесь, бережіть себе і будьте бережні, як будете їхати додомку ❤️'
+        '\n\nВав! Маю надію ви всі добре почуваєтесь, бережіть себе і будьте бережні, як будете їхати додомку ❤️\n'
+        f"{get_weather()}"
     )
 
     assert parse_total_kassa(example_total3) == (
@@ -32,5 +34,6 @@ def test_total_tip():
         '\nНе сходиться z-звіт з айко продажем на:474.5'
         '\n\nYa perdolive'
         '\nВав! Новий рекорд на доставці! Був 21155 13.01.21, а тепер 131518.3'
-        '\nВав! Новий рекорд в залі ретсорану! Був 31845 26.12, а тепер 111490.0'
+        '\nВав! Новий рекорд в залі ретсорану! Був 31845 26.12, а тепер 111490.0\n'
+        f"{get_weather()}"
     )
