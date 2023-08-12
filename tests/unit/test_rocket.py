113164aeeb2a5fe59c0fc9305361ee779ca99a75
Andriy Zhyhil <andriyzhyhil@Andriys-MacBook-Pro-Home.local>
2023-08-12 13:06:37 +0300

    handle almost record cases

--- tests/unit/test_rocket.py
+++ tests/unit/test_rocket.py
@@ -3,7 +3,8 @@ from http import HTTPStatus
 
 from tests.unit.data.rocket_order_examples import (example1, example_total1,
                                                    example_total2,
-                                                   example_total3, resto_record_data, delivery_record_data)
+                                                   example_total3, resto_record_data,
+                                                   delivery_record_data, example_total4)
 from utils.rocket import parse_rocket_fmt, parse_total_kassa
 from utils.weather_cli import get_whether_forecast
 
@@ -50,10 +51,23 @@ def test_total_tip(mocker):
         '\nВав! Новий рекорд в залі ретсорану! Був 31845 26.12.2022, а тепер 111490.0\n'
         f"{get_whether_forecast()}"
     )
+    assert parse_total_kassa(example_total4, 'dev') == (
+        'Каса 2023-05-13 - ВП: 39105 - 40%\n'
+        '(МТ ВП: 10800 73%)\n'
+        'Доставка: 35000\n'
+        'Зал ресторану: 30000\n'
+        'Разом: 65000\n'
+        '\n'
+        'Ya perdolive\n'
+        'До рекорду в залі ресторану недотягнули.Досі рекорд 31845, якби мали ще 2 '
+        'чека - то мали б новий\n'
+    )
 
 
 def test_total_none_previous_week(mocker):
     mocker.patch('utils.rocket.get_previous_date_total', return_value=None)
+    mock_gspread = mocker.patch('utils.rocket.get_records')
+    mock_gspread.return_value = delivery_record_data, resto_record_data
     assert parse_total_kassa(example_total1, 'dev') == (
         'Каса 2021-05-13 - ВП: 7083 - 39%\n'
         'Доставка: 3719\n'
@@ -67,6 +81,8 @@ def test_total_none_previous_week(mocker):
 def test_total_api_exception_previous_week(mocker):
     prev = mocker.patch('utils.gspread_api.gspread.service_account', return_value=None)
     prev.side_effect = Exception(HTTPStatus.GATEWAY_TIMEOUT)
+    mock_gspread = mocker.patch('utils.rocket.get_records')
+    mock_gspread.return_value = {}, {}
     assert parse_total_kassa(example_total1, 'dev') == (
         'Каса 2021-05-13 - ВП: 7083 - 39%\n'
         'Доставка: 3719\n'
